cdcb21ae1c694932c6f901a17e359c66215de3ec
CoJleBaP <151513830+CoJleBaP@users.noreply.github.com>
2023-12-25 01:44:15 +0300

    Попытка вставить измененный репозиторий

---
+++ Emelya_patient/EmelinDescriptions.h
@@ -0,0 +1,14 @@
+/**
+	@mainpage	Проект ЛР3 для тестирования Doxygen
+	Используется в лабораторной работе номер 3
+	по курсу Проектирование программного обеспечения
+	@copyright	ВоГУ
+	@author		Емелин Алексей Евгеньевич
+	@date		18-12-23
+	@version	12.22.21
+\par Использует классы:
+- @ref Emelin_Class_Patient
+\par Содержит класс:
+- @ref Emelin_Class_Patient.h
+- @ref Emelin_Class_Patient.cpp
+*/
--- Emelya_patient/Emelin_Class_Patient.cpp
+++ Emelya_patient/Emelin_Class_Patient.cpp
@@ -1,6 +1,18 @@
 #include "stdafx.h"
 #include "Emelin_Class_Patient.h"
 
+/**
+	@file		Emelin_Class_Patient.h
+	@brief		Заголовочный файл класса Пациент
+	@copyright	ВоГУ
+	@author		Емелин Алексей Евгеньевич
+	@date		18-12-23
+	@version	12.22.21
+\par Использует классы:
+	@ref Emelin_Class_Patient
+\par Содержит класс:
+	@ref Emelin_Class_Patient
+*/
 
 Emelin_Class_Patient::Emelin_Class_Patient()
 {
